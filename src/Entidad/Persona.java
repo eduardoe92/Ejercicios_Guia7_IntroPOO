f947d9ac674ed9e20bc1f0243f05dc240c9c6c15
eduardoe92 <eduardo.elias.1192@gmail.com>
2023-04-12 02:38:28 -0300

    Actualizacion de ejercicios

--- src/Entidad/Persona.java
+++ src/Entidad/Persona.java
@@ -5,10 +5,83 @@ public class Persona {
     
     public String nombre;
     public String apellido;
-    public int edad;
     public String ciudad;
     public String calle;
     public int altura;
     public int dni;
-   
+    public int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String ciudad, String calle, int altura, int dni, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.calle = calle;
+        this.altura = altura;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", ciudad=" + ciudad + ", calle=" + calle + ", altura=" + altura + ", dni=" + dni + ", edad=" + edad + '}';
+    }
 }
--- src/Guía6/Personas.java
+++ src/Guía6/Personas.java
@@ -1,16 +1,56 @@
-
 package Guía6;
 
 import Entidad.Persona;
+import java.util.Scanner;
 
 public class Personas {
 
     public static void main(String[] args) {
-        
-        Persona p1 = new Persona();
-        
-        
-        
-    }
 
+        Scanner leer = new Scanner(System.in);
+        Persona p = new Persona();
+        
+        System.out.println("Ingresa la cantidad de personas que deseas agregar al sistema: ");
+        int cantidad = (int) leer.nextInt();
+        leer.nextLine();
+        
+        for (int i = 1; i <= cantidad; i++) {
+                      
+            System.out.println("=======================================");
+            System.out.println("Ingresa los datos de la persona " + i);
+            System.out.println("=======================================");
+            
+            System.out.print("Nombre: ");
+            p.setNombre(leer.nextLine());
+
+            System.out.print("Apellido: ");
+            p.setApellido(leer.nextLine());
+
+            System.out.print("Ciudad: ");
+            p.setCiudad(leer.nextLine());
+
+            System.out.print("Calle: ");
+            p.setCalle(leer.nextLine());
+
+            System.out.print("Altura: ");
+            p.setAltura(leer.nextInt());
+
+            System.out.print("DNI: ");
+            p.setDni(leer.nextInt());
+
+            System.out.print("Edad: ");
+            p.setEdad(leer.nextInt());
+            leer.nextLine();
+
+            System.out.println("=============================");
+            System.out.println("Los datos ingresados de la persona son: ");
+            System.out.println("Nombre: " + p.getNombre());
+            System.out.println("Apellido: " + p.getApellido());
+            System.out.println("Ciudad: " + p.getCiudad());
+            System.out.println("Calle: " + p.getCalle());
+            System.out.println("Altura: " + p.getAltura());
+            System.out.println("DNI: " + p.getDni());
+            System.out.println("Edad: " + p.getEdad());
+        }
+    }
 }
